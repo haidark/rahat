86b15cb14624a0cd5bab43e664e1d3e86ceda923
Haidar Khan <haidark@gmail.com>
2014-04-24 12:08:18 -0400

    Plotted Location data on the map

---
+++ www/DBhelpers.php
@@ -0,0 +1,17 @@
+<?php
+function getNodesByPhrase($mysqli, $phrase){
+		$rows = $mysqli->query("SELECT nID, devID FROM nodes WHERE session='{$phrase}'");
+		for($rowNum = 0; $rowNum < $rows->num_rows; $rowNum++){
+			$nodes[$rowNum] = $rows->fetch_assoc();
+		}
+		return $nodes;
+	}
+
+function getLocationsByPhrase_NID($mysqli, $phrase, $nID){
+	$rows = $mysqli->query("SELECT time, lat, lon FROM {$phrase} WHERE nodeID='{$nID}'");
+	for($rowNum = 0; $rowNum < $rows->num_rows; $rowNum++){
+		$locations[$rowNum] = $rows->fetch_assoc();
+	}
+	return $locations;
+}
+?>
--- www/monitor2.php
+++ www/monitor2.php
@@ -1,31 +1,77 @@
-<!DOCTYPE HTML>
-<html>
-<head>
-	<meta charset="UTF-8"><title>Monitor</title>
-</head>
-<body>
-<?php	
+<?php
+	include_once("GoogleMap.php");
+	include_once("JSMin.php");
+	include_once("DBhelpers.php");	
 	
+
 	$mysqli = new mysqli("127.0.0.1", "www", "pin101", "haramain2");
 	if($mysqli->connect_errno){
 		echo "<p>Failed to connect to MySQL: " . $mysqli->connect_error."</p>";
 	}
 	$phrase = 'session1';
-	$nodes = $mysqli->query("SELECT nID, devID FROM nodes WHERE session='{$phrase}'");
-		//for each node is the session
-		for($nodeNum = 0; $nodeNum < $nodes->num_rows; $nodeNum++){
-			//print out a table (caption is devID)
-			$node = $nodes->fetch_assoc();
-			echo "<table><caption>".$node["devID"]."</caption>\n";
-			echo "<tr><th>Time</th><th>Latitude</th><th>Longitude</th></tr>";
+	$nodes = getNodesByPhrase($mysqli, $phrase);
+
+	//Maps stuff
+	$MAP_OBJECT = new GoogleMapAPI(); 
+	$MAP_OBJECT->_minify_js = isset($_REQUEST["min"])?FALSE:TRUE;
+	if( $_POST != NULL){
+			$selNode = $nodes[$_POST["node"]];
 			//get all locations of the node
-			$locations = $mysqli->query("SELECT time, lat, lon FROM {$phrase} WHERE nodeID='{$node["nID"]}'");
-			for($locNum = 0; $locNum < $locations->num_rows; $locNum++){
-				$location = $locations->fetch_assoc();
-				echo "<tr><td>{$location["time"]}</td><td>{$location["lat"]}</td><td>{$location["lon"]}</td></tr>";
+			$locations = getLocationsByPhrase_NID($mysqli, $phrase, $selNode["nID"]);		
+			//TODO: plot all the locations on the map
+			$locNum = 1;
+			foreach($locations as $location){
+				$MAP_OBJECT->addMarkerByCoords(floatval($location["lon"]),floatval($location["lat"]),"Location Number {$locNum}", $location["time"]);
+				$locNum++;
 			}
-			echo "</table>";			
+	}
+?>
+<html>
+<head>
+	<meta charset="UTF-8"><title>Monitor</title>
+	<?=$MAP_OBJECT->getHeaderJS();?>
+	<?=$MAP_OBJECT->getMapJS();?>
+</head>
+<body>
+<?php	
+
+?>
+<div id="selectNode">
+		<form action="monitor2.php" method="POST">
+			<p>Select Node:</p>
+			<select name="node">
+			<?php
+			for($nodeNum = 0; $nodeNum < count($nodes); $nodeNum++){
+				$node = $nodes[$nodeNum];
+				echo "<option value=\"{$nodeNum}\"> {$node["devID"]} </option>";
+			}
+			?>
+			</select>
+			<p><input type="submit" /></p>
+		</form>
+</div>
+
+<?php		
+	if( $_POST != NULL){
+		$selNode = $nodes[$_POST["node"]];
+		//get all locations of the node
+		$locations = getLocationsByPhrase_NID($mysqli, $phrase, $selNode["nID"]);
+		
+		//plot all the locations on the map
+	
+		$MAP_OBJECT->printOnLoad();
+		$MAP_OBJECT->printMap();
+		//$MAP_OBJECT->printSidebar();
+		
+		//print out a table (caption is devID)
+		echo "<table><caption>".$selNode["devID"]."</caption>\n";
+		echo "<tr><th>Time</th><th>Latitude</th><th>Longitude</th></tr>";		
+		foreach($locations as $location){
+			echo "<tr><td>{$location["time"]}</td><td>{$location["lat"]}</td><td>{$location["lon"]}</td></tr>";
 		}
+		echo "</table>";	
+		
+	}		
 ?>
 </body>
 
