e3f346de31ac778f14ead4d461e5758302a247ae
Haidar Khan <haidark@gmail.com>
2014-04-29 15:14:01 -0400

    namespace error

--- DBhelper.py
+++ DBhelper.py
@@ -1,5 +1,3 @@
-from errors import SessionError, NodeError, FieldError
-
 ##########################################################################
 ##########################################################################
 ##################### HARAMAIN 2 FUNCTIONS ###############################
@@ -157,4 +155,63 @@ def displayNodes(cur, session=0):
 	for node in nodes:
 		print node
 
+##########################################################################
+##########################################################################
+##################### Exception Definitions ##############################
+##########################################################################
+##########################################################################
+
+class Error(Exception):
+	"""Base class for exceptions in this module."""
+	pass
+	
+class NodeError(Error):
+	"""Exception raised for errors related to nodes.
+
+	Attributes:
+	devID -- input devID for which node error occurred
+	msg  -- explanation of the error
+	"""
+	
+	def __init__(self, devID, msg):
+		self.devID = devID
+		self.msg = msg
+	
+	#Error Types
+	DNE = "Node Does Not Exist"
+	AE = "Node Already Exists"
+	ACT = "Node is Active"
+	FRE = "Node is Free"
+	
+class SessionError(Error):
+	"""Exception raised for errors related to sessions.
+
+	Attributes:
+	session -- input session for which session error occurred
+	msg  -- explanation of the error
+	"""	
+	
+	def __init__(self, session, msg):
+		self.session = session
+		self.msg = msg
+	
+	#Error types
+	DNE = "Session Does Not Exist"
+	AE = "Session Already Exists"
+		
+class FieldError(Error):
+	"""Exception raised for errors related to invalid fields.
+
+	Attributes:
+	field -- input field for which field error occurred
+	msg  -- explanation of the error
+	"""
+	
+	def __init__(self, field, msg):
+		self.field = field
+		self.msg = msg
+	
+	#Error types
+	IP = "Invalid Phrase for Session"
+	ID = "Invalid devID for Node"
 		
