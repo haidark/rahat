bc4922b85a1e3cc3f632b7d82a23ed4da43af623
Haidar Khan <haidark@gmail.com>
2014-04-29 14:39:57 -0400

    wrote tests for manager functions

--- manager.py
+++ manager.py
@@ -96,4 +96,106 @@ elif args.NODES != None:
 	print args.NODES
 	
 else:
-	parser.print_help()
+	#run a test of each function
+	#variables
+	ses = 'sessionx'
+	dev1 = 'devx'
+	dev2 = 'devy'
+	dev3 = 'devz'
+	
+	print "(========)Testing all DB managing functions(========)"
+	print "(=) Connecting to the DB..."
+	(conn, cur) = DBconnect(host,user,pw)
+	print "(+) Connected!"
+	
+	print "(========)SESSION FUNCTION TESTS(========)"	
+	print "(=) Creating a new session called '%s" % ses
+	db.createSession(cur, ses)
+	print "(+) Session created!"
+	print "(=) Testing Failure of createSession function"
+	try:
+		db.createSession(cur, ses)
+		print "\t(-) Failure of createSession Test Failed"
+	except SessionError as se:
+		print "\t(+) Failure of createSession Test Passed. Error Caught:", se.msg
+	
+	
+	print "(========)NODE FUNCTION TESTS(========)"
+	print "(=) Creating nodes"
+	d1 = db.createNode(cur, dev1, ses)
+	d2 = db.createNode(cur, dev2)
+	d3 = db.createNode(cur, dev3);
+	print "(=) Testing creation of existing node"
+	dx = db.createNode(cur, dev1, ses)
+	
+	#check if everything went well
+	if d1 == -1 or d2 == -1 or dx == -1 or d3 == -1:
+		"\t(-) Node Creation failed! Printing list of all Nodes."
+		db.displayNodes(cur)
+	else:
+		print "\t(+) Nodes created! Testing displayNodes function"
+		print "(=) List of all nodes"
+		db.displayNodes(cur)
+		print "(=) List of all nodes in session=%s" % ses
+		db.DisplayNodes(cur, ses)
+	
+	if dx == d1:
+		"\t(+) Existing Node Creation Test Passed. nIDs are the same. Warning printed"
+	else:
+		"\t(-) Existing Node Creation Test Failed. nIDs are not the same. Printing list of all Nodes."
+		db.displayNodes(cur)
+	
+	print "(=) activateNode Test"
+	
+	print "(=) activating device=%s" % dev2
+	db.activateNode(cur, dev2, ses)
+	print "(+) Activated, printing all nodes in %s" % ses
+	db.displayNodes(cur, ses)
+	
+	print "(=) Assert Free Node Test"
+	try:
+		db.activateNode(cur, dev2, ses)
+		print "\t(-) Assert Free Node Test Failed"
+	except NodeError as ne:
+		print "\t(+) Assert Free Node Test Passed. Error Caught: ", ne.msg
+		
+	print "(=) freeNode Test"
+
+	print "(=) freeing device=%s" % dev1
+	db.freeNode(cur, dev1)
+	print "(+) Freed, printing all nodes in %s" % ses
+	db.displayNodes(cur, ses)
+	
+	print "(=) Check Node State Test"
+	try:
+		db.freeNode(cur, dev2, ses)
+		print "\t(-) Check Node State Test Failed"
+	except NodeError as ne:
+		print "\t(+) Check Node State Test Passed. Error Caught: ", ne.msg
+	
+	print "(=) deleteNode Test"
+	
+	print "(=) deleting device=%s" % dev3
+	db.deleteNode(cur, dev3)
+	
+	print "(=) deleting non-existent device."
+	try:
+		db.deleteNode(cur, 'devu')
+		print "(-) deleteNode Test Failed."
+	except NodeError as ne:
+		print "(+) deleteNode Test Passed. Error Caught: ", ne.msg
+	
+	print "(=) cleaning up."
+	
+	db.deleteNode(cur, dev1)
+	db.deleteNode(cur, dev2)
+	
+	db.deleteSession(cur, ses)
+		
+	db.displayNodes(cur)	
+		
+		
+		
+		
+		
+	#parser.print_help()
